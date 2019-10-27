094ab76e476a3a1e48cae6265e0e22f3f651538f
Fred Duby <fcd1@columbia.edu>
2019-10-27 14:23:03 -0400

    WIP: emails.yml config file

--- config/initializers/amesa.rb
+++ config/initializers/amesa.rb
@@ -1,1 +1,2 @@
 ADMINS = Array.new(YAML.load_file("#{Rails.root}/config/admins.yml")[Rails.env]).freeze
+EMAILS = HashWithIndifferentAccess.new(YAML.load_file("#{Rails.root}/config/emails.yml")[Rails.env]).freeze
