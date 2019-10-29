84749ba3aa2ee7816f9614dcc39abf145c234fa7
Fred Duby <fcd1@columbia.edu>
2019-10-29 16:11:55 -0400

    Add route to match legacy url to view scholar

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   resources :pending_scholars, only: [:index, :edit]
   resources :scholar_edits, except: [:show]
   get '/scholars/regions/:id', to: 'scholars#by_region_of_study', as: 'by_regions'
+  get '/scholars/view/:id', to: 'scholars#show'
   get 'search', to: 'scholars#search', as: 'search'
   root 'pages#home'
 end
