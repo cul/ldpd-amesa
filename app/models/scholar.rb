2c67ffb3bf3ad8363874d0b4d2e3172699211e2a
Fred Duby <fcd1@columbia.edu>
2019-10-29 10:52:11 -0400

    WIP: Search

--- app/models/scholar.rb
+++ app/models/scholar.rb
@@ -6,4 +6,13 @@ class Scholar < ApplicationRecord
   validates :first_name, :last_name, :email, :publications, presence: true
 
   paginates_per 10
+
+  def self.search(search_term)
+search_query = "LOWER(first_name) = :search_term or LOWER(last_name) = :search_term or \
+LOWER(affiliation) LIKE :search_term or LOWER(countries_of_specialization) LIKE :search_term or \
+LOWER(education) LIKE :search_term or LOWER(research_interests) LIKE :search_term or \
+LOWER(teaching_interests) LIKE :search_term or LOWER(publications) LIKE :search_term or LOWER(keywords) LIKE :search_term"
+# Scholar.where("first_name = :search_term or last_name = :search_term", search_term: search_term.downcase)
+    Scholar.where(search_query, search_term: search_term.downcase)
+  end
 end
